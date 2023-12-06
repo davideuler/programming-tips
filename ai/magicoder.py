ee1d2ed027952cf41d675ef7455acc1eb76a7d1b
david ullua <davidullua@ihopeit.com>
2023-12-07 02:04:56 +0800

    magicoder

---
+++ ai/magicoder.py
@@ -0,0 +1,22 @@
+#!/bin/env python
+
+# after downloaded the Magicoder model, could serve the application 
+# download the magicoder model: git clone https://huggingface.co/ise-uiuc/Magicoder-S-DS-6.7B
+
+import gradio as gr
+from transformers import pipeline
+
+pipe = pipeline("text-generation", model="Magicoder-S-DS-6.7B")
+
+def predict(text):
+  return pipe(text)[0]["generated_text"]
+
+demo = gr.Interface(
+  fn=predict,
+  inputs='text',
+  outputs='text',
+)
+
+demo.launch(server_name="0.0.0.0", server_port=8002, share=False)
+
+
